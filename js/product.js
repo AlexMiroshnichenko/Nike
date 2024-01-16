5d88f92737141dbca04795cfc29139e567a02f46
Alex Miroshnichenko <miroshnichenko.ua@gmail.com>
2024-01-16 17:31:12 +0200

    Add script.js and product.js

---
+++ js/product.js
@@ -0,0 +1,45 @@
+'use strict';
+const product = [
+  {
+    model: 'Nike Dunk',
+    price: '16 500 ₽',
+    color: 'серый',
+    img: './images/nike-dunk.jpg',
+    rewies: 10,
+  },
+  {
+    model: 'Nike Air',
+    price: '20 500 ₽',
+    color: 'черный',
+    img: './images/nike-air.jpg',
+    rewies: 25,
+  },
+  {
+    model: 'Nike Defy',
+    price: '19 500 ₽',
+    color: 'белый',
+    img: './images/nike-defy.jpg',
+    rewies: 75,
+  },
+  {
+    model: 'Nike Air Max',
+    price: '18 900 ₽',
+    color: 'белый',
+    img: './images/nike-air-max.jpg',
+    rewies: 100,
+  },
+  {
+    model: 'Nike Varsity',
+    price: '21 000 ₽',
+    color: 'синий',
+    img: './images/nike-varsity-royal.jpg',
+    rewies: 35,
+  },
+  {
+    model: 'Nike',
+    price: '5 200 ₽',
+    color: 'белый',
+    img: './images/nike.jpg',
+    rewies: 50,
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const cardInner = document.querySelector('.card-inner');
+
+for (let i = 0; i < product.length; i++) {
+  cardInner.innerHTML += `
+    <article class="product-card">
+        <div class="product-card-img">
+            <img src="${product[i].img}" width="400" height="390" alt="Кроссовки Nike Dunk High Retro Premium" loading="lazy">
+        </div>
+        <div class="product-card-info">
+            <h3 class="product-card-title">
+                ${product[i].model}
+            </h3>
+            <span class="product-card-price">${product[i].price}</span>
+            <ul class="product-card-list">
+                <li class="product-card-item">
+                    Цвет:<span>${product[i].color}</span>
+                </li>
+                <li class="product-card-item">
+                    Отзывы:<span>${product[i].rewies}</span>
+                </li>
+            </ul>
+            <a class="product-card-link" href="#" target="_blank">Купить</a>
+        </div>
+    </article>`;
+}
